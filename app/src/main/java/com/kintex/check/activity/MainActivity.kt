6689c951040be32c610fbc5f9ed510a4c019e418
wylee <643162885@qq.com>
2020-08-31 10:44:48 +0800

    first

---
+++ app/src/main/java/com/kintex/check/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kintex.check.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kintex.check.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/kintex/check/activity/WelcomeActivity.kt
@@ -0,0 +1,16 @@
+package com.kintex.check.activity
+
+import android.os.Bundle
+import com.kintex.check.R
+
+class WelcomeActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+
+    }
+
+}
